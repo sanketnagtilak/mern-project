3a0264865a21bf3fd292f0edc1538d538774ca37
sanketnagtilak <sanketnagtilak1@gmail.com>
2025-07-25 16:02:32 +0530

    v 0.0.2

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import{BrowserRouter, Routes,Route} from 'react-router-dom';
+
+import Home from './pages/Home';
+import Signin from './pages/Signin';
+import Signup from './pages/Signup';
+import About from './pages/About';
+import Profile from './pages/Profile';
+import Header from './components/Header';
 
 export default function App() {
-  return (
-    <div>App</div>
-  )
+  return(
+      <BrowserRouter>
+      <Header/>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/signin" element={<Signin />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/profile" element={<Profile />} />
+          </Routes>
+      </BrowserRouter>
+  );
 }
